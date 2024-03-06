0384e063e6284ef2cf4ad03cdc5885d232c4b530
weluid <dekovalov@gmail.com>
2024-03-06 11:15:05 +0200

    Changes

--- lib/ui/login_page/login_page_view.dart
+++ lib/ui/login_page/login_page_view.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:maxopen_test/bloc/login_page_bloc/login_page_bloc.dart';
